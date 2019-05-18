5977b9e5a3d733b6311dc60dc5762365fbce0d38
brbocchi <bocchisolar@yahoo.com.br>
2019-05-18 14:18:03 -0300

    “done”

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
-import AuthService from './components/auth/auth-service';
+// import AuthService from './components/auth/auth-service';
 import AddOffer from './components/projects/AddOffer';
 import OfferList from './components/projects/OfferList';
-import Navbar from './components/navbar/Navbar';
+// import Navbar from './components/navbar/Navbar';
 import OfferDetails from './components/projects/OfferDetails';
 // import TaskDetails from './components/tasks/TaskDetails'; // <== import the TaskDetails component
-import Signup from './components/auth/Signup';
-import Login from './components/auth/Login';
-import ProtectedRoute from './components/auth/protected-route';
+// import Signup from './components/auth/Signup';
+// import Login from './components/auth/Login';
+// import ProtectedRoute from './components/auth/protected-route';
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = { loggedInUser: null };
-    this.service = new AuthService();
+    // this.service = new AuthService();
   }
 
   getTheUser= (userObj) => {
@@ -41,30 +41,41 @@ class App extends Component {
     }
   }
   render() {
-    {this.fetchUser()}
-    if(this.state.loggedInUser){
-      return (
-        <div className="App">
-          <Navbar userInSession={this.state.loggedInUser} getUser={this.getTheUser} />
-          <Switch>
-            <ProtectedRoute user={this.state.loggedInUser} path='/offers/:id' component={OfferDetails} />
-            <ProtectedRoute user={this.state.loggedInUser} path='/offers' component={OfferList} />
-          </Switch>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <Navbar userInSession={this.state.loggedInUser} getUser={this.getTheUser} />
-            <Switch> 
-              <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
-              <Route exact path='/' render={() => <Login getUser={this.getTheUser}/>}/>
-              <ProtectedRoute user={this.state.loggedInUser} path='/projects/:id' component={OfferDetails} />
-              <ProtectedRoute user={this.state.loggedInUser} path='/projects' component={OfferList} />
-            </Switch>
-        </div>
-      );
-    }
+    return(
+      <div className="App">
+        <Switch>
+          <Route path='/offers/:id' component={OfferDetails}/>
+          <Route path='/offers' component={OfferList}/>
+        </Switch>
+      </div>
+    )
+  
+  
+    //   {this.fetchUser()}
+  //   if(this.state.loggedInUser){
+  //     return (
+  //       <div className="App">
+  //         <Navbar userInSession={this.state.loggedInUser} getUser={this.getTheUser} />
+  //         <Switch>
+  //           <ProtectedRoute user={this.state.loggedInUser} path='/offers/:id' component={OfferDetails} />
+  //           <ProtectedRoute user={this.state.loggedInUser} path='/offers' component={OfferList} />
+  //         </Switch>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div className="App">
+  //         <Navbar userInSession={this.state.loggedInUser} getUser={this.getTheUser} />
+  //           <Switch> 
+  //             <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
+  //             <Route exact path='/' render={() => <Login getUser={this.getTheUser}/>}/>
+  //             <ProtectedRoute user={this.state.loggedInUser} path='/projects/:id' component={OfferDetails} />
+  //             <ProtectedRoute user={this.state.loggedInUser} path='/projects' component={OfferList} />
+  //           </Switch>
+  //       </div>
+  //     );
+  //   }
   }
 }
+
 export default App;
